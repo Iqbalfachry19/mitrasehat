ee97de1cacbd32c79252573b63aead9ee263646a
iqbalfachry19 <ifachry@rocketmail.com>
2022-01-04 17:58:55 +0700

    hi

--- src/pages/api/notification.js
+++ src/pages/api/notification.js
@@ -3,47 +3,49 @@ const midtransClient = require("midtrans-client");
 export default async (req, res) => {
   if (req.method === "GET") {
     res.status(200).json({ status: "success" });
-  }
-  console.log(req.body);
-  let apiClient = new midtransClient.Snap({
-    isProduction: false,
-    serverKey: process.env.NEXT_PUBLIC_MIDTRANS_SERVER_KEY,
-    clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY,
-  });
-  let notificationJson = req.body;
-  apiClient.transaction
-    .notification(notificationJson)
-    .then((statusResponse) => {
-      let orderId = statusResponse.order_id;
-      let transactionStatus = statusResponse.transaction_status;
-      let fraudStatus = statusResponse?.fraud_status;
-      console.log(
-        `Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`
-      );
+  } else if (req.method === "POST") {
+    console.log(req.body);
+    let apiClient = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: process.env.NEXT_PUBLIC_MIDTRANS_SERVER_KEY,
+      clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY,
+    });
+    let notificationJson = req.body;
+    apiClient.transaction
+      .notification(notificationJson)
+      .then((statusResponse) => {
+        let orderId = statusResponse.order_id;
+        let transactionStatus = statusResponse.transaction_status;
+        let fraudStatus = statusResponse?.fraud_status;
+        console.log(
+          `Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`
+        );
 
-      if (transactionStatus == "capture") {
-        if (fraudStatus == "challenge") {
+        if (transactionStatus == "capture") {
+          if (fraudStatus == "challenge") {
+            res.status(200);
+          } else if (fraudStatus == "accept") {
+            res.status(200);
+          }
+        } else if (transactionStatus == "settlement") {
           res.status(200);
-        } else if (fraudStatus == "accept") {
+        } else if (transactionStatus == "deny") {
+          res.status(200);
+        } else if (
+          transactionStatus == "cancel" ||
+          transactionStatus == "expire"
+        ) {
+          res.status(200);
+        } else if (transactionStatus == "pending") {
           res.status(200);
         }
-      } else if (transactionStatus == "settlement") {
         res.status(200);
-      } else if (transactionStatus == "deny") {
-        res.status(200);
-      } else if (
-        transactionStatus == "cancel" ||
-        transactionStatus == "expire"
-      ) {
-        res.status(200);
-      } else if (transactionStatus == "pending") {
-        res.status(200);
-      }
-      res.status(200);
-    });
+      });
+  }
 };
 export const config = {
   api: {
+    bodyParser: false,
     externalResolver: true,
   },
 };
