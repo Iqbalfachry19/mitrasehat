7da1ea0a46118de408a26217973a0ec9d5dc893f
iqbalfachry19 <ifachry@rocketmail.com>
2022-01-05 13:19:38 +0700

    hi

--- src/pages/api/notification.js
+++ src/pages/api/notification.js
@@ -1,4 +1,4 @@
-import { getSession } from "next-auth/react";
+import { getSession } from "next-auth/client";
 const midtransClient = require("midtrans-client");
 import * as admin from "firebase-admin";
 
